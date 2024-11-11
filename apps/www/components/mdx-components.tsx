ef5f512cf3c25dc84cc5225a2bfad72e6dfcc5bb
Prodkt <technology@prodkt.cloud>
2024-11-11 16:57:12 -0700

    steps css

--- apps/www/components/mdx-components.tsx
+++ apps/www/components/mdx-components.tsx
@@ -196,7 +196,7 @@ const components = {
       <StyleWrapper styleName={__style__}>
         <pre
           className={cn(
-            "mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border bg-zinc-950 py-4 dark:bg-zinc-900",
+            "mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border bg-background py-4",
             className
           )}
           {...props}
